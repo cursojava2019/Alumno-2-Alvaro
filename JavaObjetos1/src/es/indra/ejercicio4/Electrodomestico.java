7c36c844f75ee231f258272ea7f8f7e77904d354
Alvaro <alvarojimenez95@hotmail.com>
2019-01-10 19:20:32 +0100

    Ejercicio 4 (Incompleto)

---
+++ JavaObjetos1/src/es/indra/ejercicio4/Electrodomestico.java
@@ -0,0 +1,9 @@
+package es.indra.ejercicio4;
+
+public class Electrodomestico {
+	static String COLOR_BASE = "Blanco";
+	protected Float precioBase;
+	protected String color;
+	protected Character clasificacion;
+	protected Float peso;
+}
