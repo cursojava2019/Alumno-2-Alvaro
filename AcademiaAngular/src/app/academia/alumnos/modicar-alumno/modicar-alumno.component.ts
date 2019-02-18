21ce66c2fbc90862a7ca15f3ecd4d0849236a5dc
CURSOJAVA <alvarojimenez95@hotmail.com>
2019-02-18 19:22:39 +0100

    Actualizacion: implementado servicio REST en Alumno

--- AcademiaAngular/src/app/academia/alumnos/modicar-alumno/modicar-alumno.component.ts
+++ AcademiaAngular/src/app/academia/alumnos/modicar-alumno/modicar-alumno.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Alumno } from 'src/app/shared/entities/alumno';
+import { AlumnoService } from 'src/app/shared/services/alumno.service';
+import { routerTransition } from 'src/app/router.animations';
 
 @Component({
   selector: 'app-modicar-alumno',
   templateUrl: './modicar-alumno.component.html',
-  styleUrls: ['./modicar-alumno.component.scss']
+  styleUrls: ['./modicar-alumno.component.scss'],
+  animations: [routerTransition()]
 })
 export class ModicarAlumnoComponent implements OnInit {
+  alumnoActual: Alumno;
+  constructor(private rutaActiva: ActivatedRoute, private alumnoService: AlumnoService, private router: Router) {
 
-  constructor() { }
+
+  }
 
   ngOnInit() {
+    let id = this.rutaActiva.snapshot.params.idAlumno;
+    this.alumnoService.findById(id).subscribe(data=>{
+      this.alumnoActual=data;
+      console.log(this.alumnoActual);
+    })
+
+
+  }
+
+  modificar(a: Alumno){
+    this.alumnoService.modificar(a).subscribe(data=>{
+      this.router.navigate(['alumnos']);
+    });
+
   }
 
 }
